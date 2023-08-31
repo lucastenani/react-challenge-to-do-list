3c6cf98b0096a957d624cd0a2c0c9afba012434d
lucastenani <lucas.tenani3@gmail.com>
2023-08-31 19:38:28 -0300

    header component created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./global.css";
 import styles from "./App.module.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <main className={styles.main}></main>
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import RocketIcon from "../assets/rocket.svg";
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={RocketIcon} alt="Rocket icon" title="Rocket icon" />
+      <h1>
+        to<span>do</span>
+      </h1>
+    </header>
+  );
+}
