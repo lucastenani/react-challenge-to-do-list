9e4630834e755861de228f63287c7e4c42699e15
lucastenani <lucas.tenani3@gmail.com>
2023-09-04 19:53:43 -0300

    new task form created but not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import "./global.css";
-import styles from "./App.module.css";
 import { Header } from "./components/Header";
+import { PlusCircle } from "@phosphor-icons/react";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
       <Header />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <form className={styles.taskForm}>
+          <input type="text" placeholder="Add a new task" />
+          <button className={styles.createTaskButton}>
+            <p>Create</p>
+            <PlusCircle size={16} color="#F2F2F2" />
+          </button>
+        </form>
+      </main>
     </>
   );
 }
