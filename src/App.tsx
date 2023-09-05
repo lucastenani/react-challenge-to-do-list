7e257bf4524d3fac4cb67c66c6f153ffdb29bb96
lucastenani <lucas.tenani3@gmail.com>
2023-09-05 18:39:05 -0300

    All screen styling finalized but still not functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./global.css";
 import { Header } from "./components/Header";
 import { PlusCircle } from "@phosphor-icons/react";
+import ClipBoard from "./assets/clipboard.svg";
 import styles from "./App.module.css";
 
 function App() {
@@ -15,6 +16,27 @@ function App() {
             <PlusCircle size={16} color="#F2F2F2" />
           </button>
         </form>
+
+        <section>
+          <header className={styles.tasksHeader}>
+            <article className={styles.tasksCreated}>
+              <p className={styles.tasksCreatedText}>Created tasks</p>
+              <p className={styles.tasksAmount}>0</p>
+            </article>
+            <article className={styles.tasksCompleted}>
+              <p className={styles.tasksCompletedText}>Completed</p>
+              <p className={styles.tasksAmount}>0</p>
+            </article>
+          </header>
+
+          <main className={styles.tasksMainEmpty}>
+            <img src={ClipBoard} alt="Clipboard" />
+            <article>
+              <strong>You don't have tasks registered yet</strong>
+              <p>Create tasks and organize your to-do items</p>
+            </article>
+          </main>
+        </section>
       </main>
     </>
   );
